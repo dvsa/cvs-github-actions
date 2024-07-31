4940749dc5a81e6ef4a1ad8f3dad0174155069b8
TaylorHalf <taylor.halfyard@dvsa.gov.uk>
2024-07-31 13:39:35 +0100

    Updated index.js to produce file

--- table-purge/index.js
+++ table-purge/index.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core');
 const { DynamoDBClient, DescribeTableCommand } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient, ScanCommand, BatchWriteCommand } = require('@aws-sdk/lib-dynamodb');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 async function run() {
     try {
@@ -42,6 +45,12 @@ async function run() {
         };
 
         let scanResult;
+        let batchFiles = [];
+        let batchIndex = 0;
+
+        // Create a temporary directory for batch files
+        const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'batch-'));
+
         do {
             scanResult = await docClient.send(new ScanCommand(scanParams));
 
@@ -66,21 +75,38 @@ async function run() {
                     };
                 });
 
-                if (dryRun) {
-                    console.log(`Dry run - would delete batch with keys: ${JSON.stringify(deleteRequests)}`);
-                } else {
-                    console.log(`Deleting batch with keys: ${JSON.stringify(deleteRequests)}`);
-                    await docClient.send(new BatchWriteCommand({
-                        RequestItems: {
-                            [tableName]: deleteRequests
-                        }
-                    }));
-                }
+                // Save batch to a file
+                const batchFileName = path.join(tempDir, `batch_${batchIndex}.json`);
+                fs.writeFileSync(batchFileName, JSON.stringify(deleteRequests));
+                batchFiles.push(batchFileName);
+                batchIndex++;
             }
 
             scanParams.ExclusiveStartKey = scanResult.LastEvaluatedKey;
 
         } while (typeof scanResult.LastEvaluatedKey !== 'undefined');
+
+        // Process batch files for deletion
+        for (const batchFile of batchFiles) {
+            const deleteRequests = JSON.parse(fs.readFileSync(batchFile, 'utf8'));
+            if (dryRun) {
+                console.log(`Dry run - would delete batch with keys: ${JSON.stringify(deleteRequests)}`);
+                await core.summary.addHeading('Dry run - Batch Deletion');
+                await core.summary.addRaw(`Would delete batch with keys: \n\`\`\`json\n${JSON.stringify(deleteRequests, null, 2)}\n\`\`\``);
+            } else {
+                console.log(`Deleting batch with keys: ${JSON.stringify(deleteRequests)}`);
+                await docClient.send(new BatchWriteCommand({
+                    RequestItems: {
+                        [tableName]: deleteRequests
+                    }
+                }));
+                await core.summary.addHeading('Batch Deletion');
+                await core.summary.addRaw(`Deleted batch with keys: \n\`\`\`json\n${JSON.stringify(deleteRequests, null, 2)}\n\`\`\``);
+            }
+        }
+
+        // Write summary
+        await core.summary.write();
 
     } catch (error) {
         core.setFailed(`Action failed with error ${error}`);
